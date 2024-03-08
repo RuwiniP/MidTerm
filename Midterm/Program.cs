b4593490e85084de31603aa1d38f5ea89a738e59
ruwiniP <perera95ruwini@gmail.com>
2024-03-08 11:38:13 -0500

    Completed code for midterm

--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -1,4 +1,5 @@
-﻿public class InventoryItem
+﻿using System;
+public class InventoryItem
 {
     // Properties
     public string ItemName { get; set; }
@@ -10,6 +11,10 @@
     public InventoryItem(string itemName, int itemId, double price, int quantityInStock)
     {
         // TODO: Initialize the properties with the values passed to the constructor.
+        ItemName = itemName;
+        ItemId = itemId;
+        Price = price;
+        QuantityInStock = quantityInStock;
     }
 
     // Methods
@@ -18,12 +23,16 @@
     public void UpdatePrice(double newPrice)
     {
         // TODO: Update the item's price with the new price.
+        this.Price = newPrice;
+
     }
 
     // Restock the item
     public void RestockItem(int additionalQuantity)
     {
         // TODO: Increase the item's stock quantity by the additional quantity.
+        this.QuantityInStock += additionalQuantity;
+       
     }
 
     // Sell an item
@@ -31,18 +40,40 @@
     {
         // TODO: Decrease the item's stock quantity by the quantity sold.
         // Make sure the stock doesn't go negative.
+
+        if (this.QuantityInStock == 0)
+        {
+            Console.WriteLine("Out of stock");
+        }
+
+        else
+        {
+            this.QuantityInStock -= quantitySold;
+        }
     }
 
     // Check if an item is in stock
     public bool IsInStock()
     {
         // TODO: Return true if the item is in stock (quantity > 0), otherwise false.
+
+        if (QuantityInStock > 0)
+        {
+            return true;
+            
+        }
+        return false;
     }
 
     // Print item details
     public void PrintDetails()
     {
         // TODO: Print the details of the item (name, id, price, and stock quantity).
+
+        Console.WriteLine($"Name:{this.ItemName}");
+        Console.WriteLine($"Id:{this.ItemId}");
+        Console.WriteLine($"Price:{this.Price}");
+        Console.WriteLine($"StockQuantity:{this.QuantityInStock}");
     }
 }
 class Program
@@ -56,10 +87,31 @@ class Program
         // TODO: Implement logic to interact with these objects.
         // Example tasks:
         // 1. Print details of all items.
-        // 2. Sell some items and then print the updated details.
-        // 3. Restock an item and print the updated details.
-        // 4. Check if an item is in stock and print a message accordingly.
+        item1.PrintDetails();
+        item2.PrintDetails();
 
-       
+        // 2. Sell some items and then print the updated details.
+        item1.SellItem(4);
+        item1.PrintDetails();
+ 
+
+        item2.SellItem(5);
+        item2.PrintDetails();
+
+        // 3. Restock an item and print the updated details.
+        item1.RestockItem(1);
+        item1.PrintDetails();
+
+        // 4. Check if an item is in stock and print a message accordingly.
+        if(item2.IsInStock()==true)
+        {
+            Console.WriteLine("Item is available");
+        }
+
+        else
+        {
+            Console.WriteLine("Item is not available");
+        }
+        
     }
 }
